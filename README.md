3cb7888ace2efa1bb80c873405acfeb7b57cb755
Geovanne-Costa <geovanne_al@hotmail.com>
2022-03-25 20:35:21 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-fofpkb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-fofpkb)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([{ path: '', component: TasksComponent }]),
+  ],
+  declarations: [AppComponent, HelloComponent, TasksComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
